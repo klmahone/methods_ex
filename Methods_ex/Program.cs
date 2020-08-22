3bed92aab3bf0b8a8240f95d3c39d40e31ca8aaa
klmahone <klmahone@gmail.com>
2020-08-21 21:58:50 -0400

    complete exercise

--- Methods_ex/Program.cs
+++ Methods_ex/Program.cs
@@ -4,9 +4,25 @@ namespace Methods_ex
 {
     class Program
     {
+            //--------------------------Exercise #2---------------------------
+        public static int Sum(int num1, int num2)
+        {
+            var answer = num1 + num2;
+            return answer;
+        }
+
+        public static int Multiply (int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
+        }
+
         static void Main(string[] args)
         {
-            //------------------------Exercise #1------------------------
+
+            var amountOfCars = Sum(2, 6);
+            var blah = Multiply(60, 2, 4);
+
+            //-------------------------Exercise #1------------------------
             //Name: Michael
             //Favorite Color: Blue
             //Favorite Animal: Walrus
@@ -31,7 +47,8 @@ namespace Methods_ex
             Console.WriteLine($"Favorite Color: {color}");
             Console.WriteLine($"Favorite Animal: {animal}");
             Console.WriteLine($"Favorite Band: {band}");
-
+                   
+                        
         }
     }
 }
