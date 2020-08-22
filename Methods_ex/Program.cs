848d93d45ee87cff0f37c0385461149e2eb95704
klmahone <klmahone@gmail.com>
2020-08-21 20:13:45 -0400

    first commit

---
+++ Methods_ex/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Methods_ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
