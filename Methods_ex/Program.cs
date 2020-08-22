5b2baa8024dcfaaf77755691267a92e82d0fd79b
klmahone <klmahone@gmail.com>
2020-08-21 20:14:39 -0400

    second commit

--- Methods_ex/Program.cs
+++ Methods_ex/Program.cs
@@ -6,7 +6,24 @@ namespace Methods_ex
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Name: Michael
+            //Favorite Color: Blue
+            //Favorite Animal: Walrus
+            //Favorite Band: The Beatles
+
+            Console.WriteLine("Hello! What is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {userName} What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is an awsome color! What is your farorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Excellent! What is your favorite band?");
+            var band = Console.ReadLine();
+
         }
     }
 }
